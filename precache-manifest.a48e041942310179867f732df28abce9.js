2978c6d5cb2d1e08f6e13b7ec15f5463491f2242
Monickha Rajan <monickhausha@gmail.com>
2020-05-12 23:17:41 +0530

    Updates

---
+++ precache-manifest.a48e041942310179867f732df28abce9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab451f09388b5f33ad747110dbb271f",
+    "url": "/FaceDetect/index.html"
+  },
+  {
+    "revision": "f173c7a54d5f8d4d28fb",
+    "url": "/FaceDetect/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "2864e9a427ead54cf956",
+    "url": "/FaceDetect/static/css/main.c6e26a1f.chunk.css"
+  },
+  {
+    "revision": "f173c7a54d5f8d4d28fb",
+    "url": "/FaceDetect/static/js/2.34646761.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceDetect/static/js/2.34646761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2864e9a427ead54cf956",
+    "url": "/FaceDetect/static/js/main.e2898e72.chunk.js"
+  },
+  {
+    "revision": "416eca187495a7a4b6ec",
+    "url": "/FaceDetect/static/js/runtime-main.12a0d439.js"
+  }
+]);
